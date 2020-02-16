6952880652e72f3b360e6133cb4bfc6b29e88347
Ryan Rooijakkers <R.Rooijakkers@student.tudelft.nl>
2020-02-16 22:12:22 +0100

    First commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <h1>{{ sayHello() }}</h1>
+    <h1>{{ test }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -33,8 +35,18 @@
 <script>
 export default {
   name: 'HelloWorld',
+  data() {
+    return {
+      test: 'test'
+    }
+  },
   props: {
     msg: String
+  },
+  methods: {
+    sayHello() {
+      return 'Hello!';
+    }
   }
 }
 </script>
